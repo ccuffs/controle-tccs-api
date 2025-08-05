dab32d467ef170dc4137d92a687b1f2c0c5aa5d8
giancds <gian.salton@gmail.com>
2025-08-05 17:22:24 -0300

    Adicionado modelo para controlar disponibilidade de bancas por parte dos docentes.

---
+++ src/models/docente-disponibilidade-banca.js
@@ -0,0 +1,108 @@
+"use strict";
+
+module.exports = (sequelize, DataTypes) => {
+	const DocenteDisponibilidadeBanca = sequelize.define(
+		"DocenteDisponibilidadeBanca",
+		{
+			ano: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				primaryKey: true,
+			},
+			semestre: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				primaryKey: true,
+			},
+			id_curso: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				primaryKey: true,
+			},
+			fase: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				primaryKey: true,
+			},
+			codigo: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				primaryKey: true,
+			},
+			data_defesa: {
+				type: DataTypes.DATEONLY,
+				allowNull: false,
+				primaryKey: true,
+			},
+			hora_defesa: {
+				type: DataTypes.TIME,
+				allowNull: false,
+				primaryKey: true,
+			},
+			disponivel: {
+				type: DataTypes.BOOLEAN,
+				allowNull: false,
+				defaultValue: false,
+			},
+		},
+		{
+			tableName: "docente_disponibilidade_banca",
+			schema: "tccs",
+			freezeTableName: true,
+			timestamps: false,
+		},
+	);
+
+	DocenteDisponibilidadeBanca.associate = (models) => {
+		// Relacionamento com OfertaTcc (chave estrangeira composta)
+		DocenteDisponibilidadeBanca.belongsTo(models.OfertaTcc, {
+			foreignKey: {
+				name: "ano",
+				field: "ano",
+			},
+			targetKey: "ano",
+			constraints: false,
+		});
+
+		DocenteDisponibilidadeBanca.belongsTo(models.OfertaTcc, {
+			foreignKey: {
+				name: "semestre",
+				field: "semestre",
+			},
+			targetKey: "semestre",
+			constraints: false,
+		});
+
+		DocenteDisponibilidadeBanca.belongsTo(models.OfertaTcc, {
+			foreignKey: {
+				name: "id_curso",
+				field: "id_curso",
+			},
+			targetKey: "id_curso",
+			constraints: false,
+		});
+
+		DocenteDisponibilidadeBanca.belongsTo(models.OfertaTcc, {
+			foreignKey: {
+				name: "fase",
+				field: "fase",
+			},
+			targetKey: "fase",
+			constraints: false,
+		});
+
+		// Relacionamento com Docente
+		DocenteDisponibilidadeBanca.belongsTo(models.Docente, {
+			foreignKey: "codigo",
+			targetKey: "codigo",
+		});
+
+		// Relacionamento com Curso
+		DocenteDisponibilidadeBanca.belongsTo(models.Curso, {
+			foreignKey: "id_curso",
+			targetKey: "id",
+		});
+	};
+
+	return DocenteDisponibilidadeBanca;
+};
