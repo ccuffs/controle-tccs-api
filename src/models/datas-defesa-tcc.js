bcb5f906c28544c8851fbbfb07c22a9a54b0c1df
giancds <gian.salton@gmail.com>
2025-08-05 16:56:41 -0300

    Criado um modelo para data-defesa-tcc.

---
+++ src/models/datas-defesa-tcc.js
@@ -0,0 +1,90 @@
+"use strict";
+
+module.exports = (sequelize, DataTypes) => {
+	const DatasDefesaTcc = sequelize.define(
+		"DatasDefesaTcc",
+		{
+			ano: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				primaryKey: true,
+			},
+			semestre: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				primaryKey: true,
+			},
+			id_curso: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				primaryKey: true,
+			},
+			fase: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				primaryKey: true,
+			},
+			inicio: {
+				type: DataTypes.DATEONLY,
+				allowNull: true,
+			},
+			fim: {
+				type: DataTypes.DATEONLY,
+				allowNull: true,
+			},
+		},
+		{
+			tableName: "datas_defesa_tccs",
+			schema: "tccs",
+			freezeTableName: true,
+			timestamps: false,
+		},
+	);
+
+	DatasDefesaTcc.associate = (models) => {
+		// Relacionamento com OfertaTcc (chave estrangeira composta)
+		DatasDefesaTcc.belongsTo(models.OfertaTcc, {
+			foreignKey: {
+				name: "ano",
+				field: "ano",
+			},
+			targetKey: "ano",
+			constraints: false,
+		});
+
+		DatasDefesaTcc.belongsTo(models.OfertaTcc, {
+			foreignKey: {
+				name: "semestre",
+				field: "semestre",
+			},
+			targetKey: "semestre",
+			constraints: false,
+		});
+
+		DatasDefesaTcc.belongsTo(models.OfertaTcc, {
+			foreignKey: {
+				name: "id_curso",
+				field: "id_curso",
+			},
+			targetKey: "id_curso",
+			constraints: false,
+		});
+
+		DatasDefesaTcc.belongsTo(models.OfertaTcc, {
+			foreignKey: {
+				name: "fase",
+				field: "fase",
+			},
+			targetKey: "fase",
+			constraints: false,
+		});
+
+		// Relacionamento com Curso
+		DatasDefesaTcc.belongsTo(models.Curso, {
+			foreignKey: "id_curso",
+			targetKey: "id",
+		});
+	};
+
+	return DatasDefesaTcc;
+};
